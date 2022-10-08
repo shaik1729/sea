cdb9898d3d40e518ee0b5139dae0fd695ebb13f8
shaik1729 <shaiktajuddinshavali@gmail.com>
2022-10-08 20:06:42 +0530

    users and results exception handing implemented

--- app/models/create_users_via_csv.rb
+++ app/models/create_users_via_csv.rb
@@ -3,31 +3,35 @@ require 'csv'
 class CreateUsersViaCsv
   class << self
     def convert_save(model_name, csv_data)
-      csv_file = csv_data.read
-      CSV.parse(csv_file, headers: true) do |row|
-        fields = row.to_hash
+      begin
+        csv_file = csv_data.read
+        CSV.parse(csv_file, headers: true) do |row|
+          fields = row.to_hash
 
-        email = fields["Email Address"]
-        name = fields["Name"].upcase!
-        mobile = fields["Mobile Number"]
-        rollno = fields["Roll Number"].upcase!
-        department_id = Department.find_by(code: fields["Department"].upcase!).id
-        course_id = Course.find_by(code: fields["Course"].upcase!).id
-        if fields["Regulation"]
-          regulation_id = Regulation.find_by(code: fields["Regulation"].upcase!).id
-          role_id = Role.find_by(code: "STU").id
-        else
-          role_id = Role.find_by(code: "FAC").id
+          email = fields["Email Address"]
+          name = fields["Name"].upcase!
+          mobile = fields["Mobile Number"]
+          rollno = fields["Roll Number"].upcase!
+          department_id = Department.find_by(code: fields["Department"].upcase!).id
+          course_id = Course.find_by(code: fields["Course"].upcase!).id
+          if fields["Regulation"]
+            regulation_id = Regulation.find_by(code: fields["Regulation"].upcase!).id
+            role_id = Role.find_by(code: "STU").id
+          else
+            role_id = Role.find_by(code: "FAC").id
+          end
+          batch_id = Batch.find_by(name: fields["Batch"]).id
+          avatar_url = fields["pass port photo"].split("=").last
+          password = (0...8).map { (65 + rand(26)).chr }.join
+
+          User.create!(email: email, name: name, mobile: mobile, rollno: rollno, department_id: department_id, course_id: course_id, regulation_id: regulation_id, batch_id: batch_id, avatar_url: avatar_url, role_id: role_id, password: password, password_confirmation: password)
+
+          puts "User #{name} #{rollno} #{email} #{password} created successfully!!!!"
+
+          WelcomeMailer.with(name: name, email: email, password: password).welcome_user.deliver_later
         end
-        batch_id = Batch.find_by(name: fields["Batch"]).id
-        avatar_url = fields["pass port photo"].split("=").last
-        password = (0...8).map { (65 + rand(26)).chr }.join
-
-        User.create!(email: email, name: name, mobile: mobile, rollno: rollno, department_id: department_id, course_id: course_id, regulation_id: regulation_id, batch_id: batch_id, avatar_url: avatar_url, role_id: role_id, password: password, password_confirmation: password)
-
-        puts "User #{name} #{rollno} #{email} #{password} created successfully!!!!"
-
-        WelcomeMailer.with(name: name, email: email, password: password).welcome_user.deliver_later
+      rescue => exception
+        puts "\n\n\n*********************#{exception} occured in users creation*********************\n\n\n"
       end
     end
   end
--- app/models/result.rb
+++ app/models/result.rb
@@ -11,26 +11,30 @@ class Result < ApplicationRecord
     end
 
     def self.import(file, current_user_id)
-        records = CSV.parse(File.read(file), headers: true)
-        records.each do |student_record|
-            arr_of_stu_record = student_record.to_a
-            puts "Uploading result of : #{arr_of_stu_record[0]}"
-            (3..arr_of_stu_record.length-1).step(6).each do |index| 
-                Result.create!(
-                subject_title: arr_of_stu_record[index].first.upcase,
-                internal_marks: arr_of_stu_record[index+1].last,
-                external_marks: arr_of_stu_record[index+2].last,
-                total_marks: arr_of_stu_record[index+3].last,
-                result: arr_of_stu_record[index].last.upcase == "P" ? "PASS" : "FAIL",
-                credits: arr_of_stu_record[index+4].last,
-                grade: arr_of_stu_record[index+5].last,
-                result_type: "REGULAR",
-                user_id: User.find_by(rollno: arr_of_stu_record[0].last.upcase).id,
-                semester_id: Semester.find_by(id: arr_of_stu_record[2].last.upcase).id,
-                last_updated_user_id: current_user_id
-                )
+        begin
+            records = CSV.parse(File.read(file), headers: true)
+            records.each do |student_record|
+                arr_of_stu_record = student_record.to_a
+                puts "Uploading result of : #{arr_of_stu_record[0]}"
+                (3..arr_of_stu_record.length-1).step(6).each do |index| 
+                    Result.create!(
+                    subject_title: arr_of_stu_record[index].first.upcase,
+                    internal_marks: arr_of_stu_record[index+1].last,
+                    external_marks: arr_of_stu_record[index+2].last,
+                    total_marks: arr_of_stu_record[index+3].last,
+                    result: arr_of_stu_record[index].last.upcase == "P" ? "PASS" : "FAIL",
+                    credits: arr_of_stu_record[index+4].last,
+                    grade: arr_of_stu_record[index+5].last,
+                    result_type: "REGULAR",
+                    user_id: User.find_by(rollno: arr_of_stu_record[0].last.upcase).id,
+                    semester_id: Semester.find_by(id: arr_of_stu_record[2].last.upcase).id,
+                    last_updated_user_id: current_user_id
+                    )
+                end
+                puts "#{arr_of_stu_record[0]} -> #{arr_of_stu_record[1]} result Uploaded successfully."
             end
-            puts "#{arr_of_stu_record[0]} -> #{arr_of_stu_record[1]} result Uploaded successfully."
+        rescue => exception
+            puts "\n\n\n*********************#{exception} occured in result creation*********************\n\n\n"
         end
     end
 end
