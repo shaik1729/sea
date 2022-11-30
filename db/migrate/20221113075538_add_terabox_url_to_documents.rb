e4a1155f4f2b294d4cc94ff2935dd980802d2f82
shaik1729 <shaiktajuddinshavali@gmail.com>
2022-11-30 21:17:01 +0530

    sqlite to postgresql changed and migration error of fields is solved

--- db/migrate/20221113075538_add_terabox_url_to_documents.rb
+++ db/migrate/20221113075538_add_terabox_url_to_documents.rb
@@ -1,5 +1,5 @@
 class AddTeraboxUrlToDocuments < ActiveRecord::Migration[6.1]
   def change
-    add_column :documents, :reference_url, :string
+    add_column :documents, :terabox_url, :string
   end
 end
--- db/migrate/20221130130346_fix_column_name.rb
+++ db/migrate/20221130130346_fix_column_name.rb
@@ -1,7 +1,7 @@
 class FixColumnName < ActiveRecord::Migration[6.1]
   def self.up
-    rename_column :videos, :video_url, :video_url
-    rename_column :articles, :reference_url, :reference_url
-    rename_column :documents, :reference_url, :reference_url
+    rename_column :videos, :terabox_video_url, :video_url
+    rename_column :articles, :terabox_url, :reference_url
+    rename_column :documents, :terabox_url, :reference_url
   end
 end
